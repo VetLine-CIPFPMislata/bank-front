37b36a86b409bb416dde65ca95d1b0a544e164bd
alvaro <alvaro@micorreo.com>
2026-01-08 10:31:02 +0100

    feat(VetLine-sprint2#10)Create header

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,34 @@
 import { Routes } from '@angular/router';
+import { Movimientos } from './components/movimientos/movimientos';
+import { Cuentas } from './components/cuentas/cuentas';
+import { Inicio } from './components/inicio/inicio';
+import { Tarjetas } from './components/tarjetas/tarjetas';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full'
+    },
+    {
+        path: 'home',
+        component: Inicio,
+    },
+    {
+        path: 'cuentas',
+        component: Cuentas,
+    },
+    {
+        path: 'tarjetas',
+        component: Tarjetas,
+    },
+    {
+        path: 'movimientos-bancarios',
+        component: Movimientos,
+    },
+    {
+        path: '**',
+        redirectTo: 'home',
+        pathMatch: 'full'
+    }
+];
