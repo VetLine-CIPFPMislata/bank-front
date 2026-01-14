5042957f38d364f5aac6546933a81470de9bcc2d
Adrian <ositiredulo@gmail.com>
2026-01-14 11:45:59 +0100

    fix(VetLine/sprint2#23)fix Impl of login

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,13 +3,13 @@ import { provideRouter } from '@angular/router';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 
 import { routes } from './app.routes';
-import { authInterceptor } from './interceptors/auth.interceptor';
+// import { authInterceptor } from './interceptors/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient(withInterceptors([authInterceptor]))
+    provideHttpClient()
   ]
 };
